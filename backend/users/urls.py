8e9ea66a6514fa3ffd9742ffc789899aa63d4149
AllanOcung <allansocung137@gmail.com>
2025-10-23 14:46:33 +0300

    Added sentry to backend

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -11,8 +11,8 @@ router.register(r"users", views.UserViewSet, basename="user")
 app_name = "users"
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
+# def trigger_error(request):
+#     division_by_zero = 1 / 0
 
 
 urlpatterns = [
@@ -20,7 +20,7 @@ urlpatterns = [
     path("auth/register/", views.UserRegistrationView.as_view(), name="register"),
     path("auth/login/", views.UserLoginView.as_view(), name="login"),
     path("auth/logout/", views.logout, name="logout"),
-    path("sentry-debug/", trigger_error),
+    # path("sentry-debug/", trigger_error),
     # Password reset endpoints
     path(
         "auth/password-reset/",
