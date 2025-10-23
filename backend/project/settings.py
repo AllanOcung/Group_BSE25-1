6c7a58ccc26d9d9e70212907f8e75e8d13655bea
AllanOcung <allansocung137@gmail.com>
2025-10-23 15:18:31 +0300

    install axios

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -165,8 +165,8 @@ SIMPLE_JWT = {
 
 # Use simple version:
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8080",
